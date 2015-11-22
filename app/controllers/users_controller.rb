5bf5b3f54c52aeb2304aad050542fe89467665c9
Mike Morris <mike.morris.wa@gmail.com>
2015-11-22 02:08:14 +0000

    Add friends stock view and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,15 +18,20 @@ class UsersController < ApplicationController
       render status: :not_found, nothing: true
     end
   end
-  
+
   def add_friend
     @friend = User.find(params[:friend])
     current_user.friendships.build(friend_id: @friend.id)
-    
+
     if current_user.save
       redirect_to my_friends_path, notice: "Friend was successfully added."
     else
       redirect_to my_friends_path, flash[:error] = "There was an error adding user as friend"
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
   end
 end
